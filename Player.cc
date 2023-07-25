7d86c9ba4adcb69307402184b5a523c0bee07225
鸫羊臾 <65632959+LzKakabird@users.noreply.github.com>
2023-07-25 18:15:42 -0400

    Update Player.cc

--- Player.cc
+++ Player.cc
@@ -1,17 +1,42 @@
 #include "player.h"
 
-char Player::char_at() { return '@'; }
+Player::Player(Map *p, int x, int y,
+               int cur_hp, int atk, int def,
+               int max_hp) : 
+               Character{p, x, y, cur_hp, atk, def},
+               max_hp{max_hp} 
+               {}
 
-void Player::attack(Character *e) {
+char Player::charAt(int x, int y) {
+    if((x == getX()) && (y == getY())){
+        return '@';
+    }else{
+        return nextLayer->charAt(x, y);
+    }
+}
+
+/*
+void Player::attack(Character *e)
+{
     int rand = ... % 2;
-    if (e->get_race() == "Halfling") {
-        if (rand == 0) e->hurt(damage(atk, pc->get_Def));
-    } else e->hurt(damage(atk, pc->get_Def));
+    if (e->get_race() == "Halfling")
+    {
+        if (rand == 0)
+            e->hurt(damage(atk, pc->get_Def));
+    }
+    else
+        e->hurt(damage(atk, pc->get_Def));
+}
+*/
+
+void Player::changeHP(int amt)
+{
+    cur_hp += amt;
+    if (cur_hp < 0)
+        cur_hp = 0;
+    else if (cur_hp > max_hp)
+        cur_hp = max_hp;
 }
 
-void Player::changeHP(int amt) {
-    cur_hp += amt;
-    if (cur_hp < 0) cur_hp = 0;
-    else if (cur_hp > max_hp) cur_hp = max_hp;
-}
+
 

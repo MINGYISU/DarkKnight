7f006f1fadbdeddcf21d28d7fd3df78a08cde68f
鸫羊臾 <65632959+LzKakabird@users.noreply.github.com>
2023-07-25 18:13:53 -0400

    Update character.h
    可以在main里面生成

--- character.h
+++ character.h
@@ -3,26 +3,35 @@
 
 #include "decorator.h"
 #include <cmath>
+#include <string>
+using namespace std;
 
-class Character: public Decorator {
+class Character : public Decorator
+{
     Character *pc;
-    int xCor, yCor, cur_hp;
     const int atk, def;
-    std::string race;
+    //string race;
+    bool toprint;
+    
+    protected:
+        int xCor, yCor, cur_hp;
 
     public:
-        ~Character();
-        bool to_print();
-        char set_char_at();
-        virtual char char_at();
-        int get_X();
-        int get_Y();
-        int get_Atk();
-        int get_Def();
-        std::string get_race();
-        virtual void changeHP();
+        Character(Map *p, int x, int y, int cur_hp,
+                  int atk, int def);
+        bool toPrint();
+        void setPrint(bool printOrNot);
+        // char set_char_at();
+        // char charAt(int x, int y) override;
+        int getX();
+        int getY();
+        int getAtk();
+        int getDef();
+        virtual string getRace() = 0;
+        virtual void changeHP(int amt);
         void move(int to_x, int to_y);
         int damage(int atker, int defer);
+        ~Character();
 };
 
 #endif

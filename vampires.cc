34352bec03f484b173f9b81f5660c7c932cb4bca
鸫羊臾 <65632959+LzKakabird@users.noreply.github.com>
2023-07-25 18:17:51 -0400

    Update vampires.cc

--- vampires.cc
+++ vampires.cc
@@ -1,21 +1,43 @@
-#include "vampires.h"
+#include "vampire.h"
 
-void Vampires::changeHP(int amt) {
+Vampire::Vampire(Map *p, int x, int y,
+                 int cur_hp, int atk, int def,
+                 int max_hp, string r) : 
+                 Player{p, x, y,
+                        cur_hp, atk, def,
+                        max_hp}, 
+                 race{r} {}
+
+void Vampire::changeHP(int amt)
+{
     cur_hp += amt;
-    if (cur_hp < 0) cur_hp = 0;
+    if (cur_hp < 0)
+        cur_hp = 0;
 }
 
-void Vampires::attack(Character *e) {
+/*
+void Vampire::attack(Character *e)
+{
     int rand = ... % 2;
     std::string er = e->get_race();
-    if ((er != "Halfling") || (er == "Halfling" && rand == 0)) {
+    if ((er != "Halfling") || (er == "Halfling" && rand == 0))
+    {
         e->hurt(damage(atk, pc->get_Def));
-        if (er == "Dwarf") changeHP(-5);
-        else changeHP(5);
+        if (er == "Dwarf")
+            changeHP(-5);
+        else
+            changeHP(5);
     }
 }
+*/
 
-
-void hurt(int dmg, std::string enemy) {
+/*
+void Vampire::hurt(int dmg, std::string enemy){
     changeHP(-dmg);
 }
+*/
+
+string Vampire::getRace(){
+    return race;
+}
+

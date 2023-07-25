d7c8e3c9f4c6768bca26a3e6a09a4856624e9d14
鸫羊臾 <65632959+LzKakabird@users.noreply.github.com>
2023-07-25 18:15:03 -0400

    Update Player.h

--- Player.h
+++ Player.h
@@ -2,17 +2,21 @@
 #define PLAYER_H
 
 #include "character.h"
-#include "potion.h"
+//#include "potion.h"
 
 class Player: public Character {
     const int max_hp;
     int gold = 0;
     
     public:
-    char char_at();
-    virtual void attack(Character *e);
-    virtual void hurt();
-    void changeHP() override;
+        Player(Map *p, int x, int y, 
+                int cur_hp, int atk, int def,
+                int max_hp);
+        char charAt(int x, int y) override;
+        // virtual void attack(Character *e);
+        // virtual void hurt();
+        void changeHP(int amt) override;
 };
 
 #endif
+

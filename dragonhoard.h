068693620556a0327deb5b6b12aa850ec16903da
MINGYI SU <125218610+MINGYISU@users.noreply.github.com>
2023-07-26 14:20:33 -0400

    made temp changes

--- dragonhoard.h
+++ dragonhoard.h
@@ -2,14 +2,18 @@
 #define DRAGOBHOARD_H
 
 #include "gold.h"
+
 using namespace std;
 class DragonHoard: public Gold {
-    bool pickability;
+    protected:
+        bool pickability;
+        Map *guardian;
+
     public:
-    DragonHoard(int x, int y, int a = 0, bool p = false); //constructor
-    bool getPickability(); // return the pickability
-    void setAmount(int a); // change the amount of gold
-    void setPickability(bool p); // change the pickability field
+        DragonHoard(int x, int y, int a = 0, bool p = false); //constructor
+        bool getPickability(); // return the pickability
+        void setAmount(int a); // change the amount of gold
+        void setPickability(bool p); // change the pickability field
 };
 
 #endif

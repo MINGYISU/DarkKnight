34f1ed16967f645b933bec666473a32d13af3895
MINGYI SU <125218610+MINGYISU@users.noreply.github.com>
2023-07-26 00:38:12 -0400

    Update Player

--- player.cc
+++ player.cc
@@ -8,34 +8,17 @@ Player::Player(Map *p, int x, int y,
                {}
 
 char Player::charAt(int x, int y) {
-    if((x == getX()) && (y == getY())){
-        return '@';
-    }else{
-        return nextLayer->charAt(x, y);
-    }
+    if((x == getX()) && (y == getY())) return '@';
+    else return nextLayer->charAt(x, y);
 }
 
-/*
-void Player::attack(Character *e)
-{
-    int rand = ... % 2;
-    if (e->get_race() == "Halfling")
-    {
-        if (rand == 0)
-            e->hurt(damage(atk, pc->get_Def));
-    }
-    else
-        e->hurt(damage(atk, pc->get_Def));
-}
-*/
+void Player::attack(Character *e) { e->hurt(damage(atk, e->getDef)); }
 
-void Player::changeHP(int amt)
-{
+
+void Player::changeHP(int amt) {
     cur_hp += amt;
-    if (cur_hp < 0)
-        cur_hp = 0;
-    else if (cur_hp > max_hp)
-        cur_hp = max_hp;
+    if (cur_hp < 0) cur_hp = 0;
+    else if (cur_hp > max_hp) cur_hp = max_hp;
 }
 
 

8956ad3149eb30cca4764fdef9791514a1c96d74
m7su <m7su@uwaterloo.ca>
2023-07-29 02:33:47 -0400

    ENEMY MOVE AI UPDATE

--- enemy.cc
+++ enemy.cc
@@ -13,3 +13,44 @@ bool Enemy::hurt(int dmg) {
     changeHP(-dmg);
     return true;
 }
+
+char Enemy::getCharAt(Map *m, int x, int y) { return m->charAt(x, y); }
+
+string Enemy::randDir(Map *m, int gox, int goy) {
+    vector<string> lst;
+    if (getCharAt(m, gox, goy - 1) == '.') lst.push_back("no");
+    if (getCharAt(m, gox, goy + 1) == '.') lst.push_back("so");
+    if (getCharAt(m, gox + 1, goy) == '.') lst.push_back("ea");
+    if (getCharAt(m, gox - 1, goy) == '.') lst.push_back("we");
+    if (getCharAt(m, gox + 1, goy - 1) == '.') lst.push_back("ne");
+    if (getCharAt(m, gox - 1, goy - 1) == '.') lst.push_back("nw");
+    if (getCharAt(m, gox + 1, goy + 1) == '.') lst.push_back("se");
+    if (getCharAt(m, gox - 1, goy + 1) == '.') lst.push_back("sw");
+
+    if (lst.empty()) return "noway";
+    else {
+        srand(time(0));
+        return lst.at(rand() % lst.size());
+    }
+}
+
+void Enemy::move(Map *m) {
+    string dir = randDir(m, xCor, yCor);
+    if (dir == "no") yCor -= 1;
+    else if (dir == "so") yCor += 1;
+    else if (dir == "ea") xCor += 1;
+    else if (dir == "we") xCor -= 1;
+    else if (dir == "ne") {
+        xCor += 1;
+        yCor -= 1;
+    } else if (dir == "nw") {
+        xCor -= 1;
+        yCor -= 1;
+    } else if (dir == "se") {
+        xCor += 1;
+        yCor += 1;
+    } else if (dir == "sw") {
+        xCor -= 1;
+        yCor += 1;
+    } 
+}
--- enemy.h
+++ enemy.h
@@ -5,12 +5,15 @@
 #include "player.h"
 
 class Enemy: public Character {
+    char getCharAt(Map *m, int x, int y);
+    string randDir(Map *m, int gox, int goy);
     protected:
         Player *pc;
     public:
         Enemy(Map *p, int x, int y, int atk, int def, int max_hp, Player *pc, std::string r);
         virtual void attack();
         virtual bool hurt(int dmg);
+        virtual void move(Map *m);
 };
 
 #endif

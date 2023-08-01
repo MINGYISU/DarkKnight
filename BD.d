54c98b538dbff0343457b759eee2b62b9474258b
j28shi <j28shi@uwaterloo.ca>
2023-07-31 20:02:13 -0400

    added fist

--- BD.d
+++
@@ -1,1 +0,0 @@
-BD.o: BD.cc BD.h PotDeco.h PotEffect.h
--- dragon.d
+++
@@ -1,3 +0,0 @@
-dragon.o: dragon.cc dragon.h enemy.h character.h decorator.h map.h \
- player.h Water.h PotEffect.h BA.h PotDeco.h WA.h BD.h WD.h sword.h \
- equipment.h pickable.h shield.h
